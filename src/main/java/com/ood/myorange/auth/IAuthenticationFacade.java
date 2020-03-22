33fd6c0f2a624f57f66a2ed1c0b3019ffcaf4aab
Chen Chen <cc126903@gmail.com>
2020-03-22 18:35:19 -0400

    change the way of getting user/admin info; add CI script

--- src/main/java/com/ood/myorange/auth/IAuthenticationFacade.java
+++ src/main/java/com/ood/myorange/auth/IAuthenticationFacade.java
@@ -1,10 +1,12 @@
 package com.ood.myorange.auth;
 
-import org.springframework.security.core.Authentication;
+import com.ood.myorange.dto.AdminInfo;
+import com.ood.myorange.dto.UserInfo;
 
 /**
  * Created by Chen on 3/18/20.
  */
 public interface IAuthenticationFacade {
-    Authentication getAuthentication();
+    UserInfo getUserInfo();
+    AdminInfo getAdminInfo();
 }
