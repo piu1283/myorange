4325d89634074d12a6b49baa58bd1da102441377
Chen Chen <cc126903@gmail.com>
2020-04-19 02:42:24 -0400

    remove an import

--- src/main/java/com/ood/myorange/constant/enumeration/Gender.java
+++ src/main/java/com/ood/myorange/constant/enumeration/Gender.java
@@ -1,8 +1,5 @@
 package com.ood.myorange.constant.enumeration;
 
-import com.ood.myorange.exception.InvalidRequestException;
-import com.sun.tools.javah.Gen;
-
 /**
  * Created by Chen on 2/25/20.
  */
